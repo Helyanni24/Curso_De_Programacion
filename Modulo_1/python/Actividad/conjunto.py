7e575e5e54844de4d287f13f22930c56fdbba571
Helyanni24 <helyannir@gmail.com>
2025-06-27 12:42:34 -0400

    .

--- Modulo_1/python/Actividad/conjunto.py
+++ Modulo_1/python/Actividad/conjunto.py
@@ -1,4 +1,14 @@
 #conjuntos
 
-miconjunto = {1, 2, 3, 4, 5}
+miconjunto = {2, 4, 8, 10, 12}
+print("Conjunto original:", miconjunto)
 
+
+miconjunto.add(6)
+print("Conjunto después de añadir el número 6:", miconjunto)
+
+for elemento in miconjunto:
+    print("Elemento del conjunto:", elemento)
+
+otroconjunto = {1, 3, 5, 7}
+print("¿mi_conjunto es igual a otro_conjunto?", miconjunto == otroconjunto)
